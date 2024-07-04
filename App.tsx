7c33135b5a3d19f4890aeaccc5ab95118789540d
kaungminkhant <kaungminkhant.official@gmail.com>
2024-07-04 09:31:10 +0630

    added sound file and migrated to ts

---
+++ App.tsx
@@ -0,0 +1,53 @@
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { Audio } from "expo-av";
+
+export default function App() {
+  const [sound, setSound] = useState<Audio.Sound>();
+
+  async function playSound() {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/music.mp3")
+    );
+    setSound(sound);
+
+    console.log("Playing Sound");
+    await sound.playAsync();
+  }
+
+  async function stopSound() {
+    console.log("Stoping Sound");
+    if (sound) {
+      await sound.stopAsync();
+    }
+  }
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Play Sound" onPress={playSound} />
+      <Button title="Stop Sound" onPress={stopSound} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
